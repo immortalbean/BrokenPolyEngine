b13d70d4d9601be94b7f6f85d238ec8377f83a59
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-04-19 22:18:03 -0400

    set up git ignore

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ resolution = (1280, 720)
 screen = pygame.display.set_mode(resolution)
 running = True
 clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    clock.tick(60)
+    screen.
