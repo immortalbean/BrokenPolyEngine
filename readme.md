7ef041d3916e7354ea9b289522032b0b3b3b6b27
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-04-22 21:00:19 -0400

    added base functions for gameobject

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Why is nothing in the engine yet?
+Currently, I am working on creating the foundation for the engine to use.
+# Can I contribute?
+No, this is a solo project.
--- scripts/core/gameobject.py
+++ scripts/core/gameobject.py
@@ -5,3 +5,9 @@ import pygame
 class gameobject():
     def __init__(self, initial_position: pygame.math.Vector2):
         self.position = initial_position
+    def begin(self):
+        # Starting function for game objects
+        pass
+    def update(self):
+        # Update function for game objects
+        pass
